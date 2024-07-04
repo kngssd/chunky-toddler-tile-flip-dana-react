4a9b91d1a4fd88cf5659e43a3de475cf16f93053
2405-007-dk <danakengessova@gmail.com>
2024-07-04 12:00:48 +0100

    create a new repo for the react app

---
+++ src/components/cards.jsx
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+  {
+    id: 2,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+  {
+    id: 3,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+  {
+    id: 4,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+  {
+    id: 5,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+  {
+    id: 6,
+    emoji: "",
+    word: "",
+    showed: false,
+  },
+];
